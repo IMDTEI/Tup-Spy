d394f92ac9bbff48c50c09ed13757a41f3b9e292
IMDTEI <contaarthurhoffmen@gmail.com>
2024-09-19 22:21:04 -0300

    Create chatAdapter.kt

---
+++ Tup-Spy-Arquitetura/chatAdapter.kt
@@ -0,0 +1,28 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatAdapter(private val messages: List<ChatMessage>) :
+    RecyclerView.Adapter<ChatAdapter.MessageViewHolder>() {
+
+    class MessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val messageTextView: TextView = view.findViewById(R.id.messageTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_message, parent, false)
+        return MessageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val message = messages[position]
+        holder.messageTextView.text = message.message
+    }
+
+    override fun getItemCount(): Int {
+        return messages.size
+    }
+    }
